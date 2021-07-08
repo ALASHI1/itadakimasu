375a7f60d0a3a919ee731fb6872f067a0374862c
ALASHI1 <alashimuyiwa@gmail.com>
2021-07-08 16:07:51 +0100

    Updates

--- food_site/settings.py
+++ food_site/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import dotenv
+
 import dj_database_url
 import environ
 env = environ.Env(DEBUG=(bool, False),)
